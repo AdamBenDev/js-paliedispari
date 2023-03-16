09c44631751bc46853e541450ed3952525f5f6f5
AdamBenDev <adamaya9732@gmail.com>
2023-03-16 16:43:03 +0100

    Even odd function

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 /*Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma*/
 
-
 let parola = prompt("Inserisci una parola");
 
 //!VERIFICHIAMO SE LA PAROLA E' PALINDROMA
@@ -20,20 +19,37 @@ if (isPalindroma(parola)) {
   console.log(parola + " non e' palindroma");
 }
 
-
 //?PARI E DISPARI
-
 
 /*L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.*/
 
 let sceltaUtente = prompt("Scegli pari o dispari");
 let numeroUtente = parseInt(prompt("Scegli un numero da 1 a 5"));
 
+//*genera un numero  random da 1 a 5
 
-//*genera un numero da random da 1 a 5 
-
-function numeroCasuale(min,max){
-    return Math.floor(Math.random() * (max - min + 1) + min );
+function numeroCasuale(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-let numeroComputer = numeroCasuale(1,5);
+let numeroComputer = numeroCasuale(1, 5);
+
+//*capire se un numero e' pari o no
+
+function isPari(numero) {
+    return numero % 2 === 0;
+}
+
+let somma = numeroUtente + numeroComputer;
+let risultato = isPari(somma) ? "pari" : "dispari";
+
+if(risultato === sceltaUtente) {
+    console.log("hai vinto");
+} else {
+    console.log("Hai vinto il computer!");
+}
+
+console.log("La somma e' " + somma);
+console.log("Il tuo numero e'" + numeroUtente);
+console.log("Il numero del computer e'" + NumeroComputer);
+console.log("Il risultato e'" + risultato);
